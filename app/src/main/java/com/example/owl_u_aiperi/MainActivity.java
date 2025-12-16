a3e18af85e91057c20815c4eec23192415e798ab
Dell <ulanovvaaperi05@gmail.com>
2025-12-16 20:34:07 +0600

    New commit

--- app/src/main/java/com/example/owl_u_aiperi/MainActivity.java
+++ app/src/main/java/com/example/owl_u_aiperi/MainActivity.java
@@ -4,22 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
-    Button btnEmail;
+    Button btnGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnEmail=findViewById(R.layout.activity_main);
-        btnEmail.setOnClickListener(v -> {
+        btnGame=findViewById(R.id.btnGame);
+        btnGame.setOnClickListener(v -> {
             Intent intent=new Intent(MainActivity.this, RegisterActivity.class);
             startActivity(intent);
         });
